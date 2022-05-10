9d435b9e43f47c3378e148894094115380cbdc3b
pixelfac <pixelfac.nerfbros@gmail.com>
2022-05-10 12:16:11 -0400

    set line collider radius

--- Assets/Scripts/ChalkLine/ChalkLine.cs
+++ Assets/Scripts/ChalkLine/ChalkLine.cs
@@ -43,6 +43,7 @@ public class ChalkLine : MonoBehaviour
 
 		//Set EdgeCollider
 		hitbox.SetPoints(nodePositions);
+		hitbox.edgeRadius = lr.startWidth;
 		//loop EdgeCollider
 		hitbox.adjacentEndPoint = nodePositions[0]; 
 		hitbox.useAdjacentEndPoint = true;
