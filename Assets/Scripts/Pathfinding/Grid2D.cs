70abe168536e7f7356204459b507787f833c1cda
pixelfac <pixelfac.nerfbros@gmail.com>
2022-05-14 14:37:56 -0400

    merge pathfinding2D into grid2D

--- Assets/Scripts/Pathfinding/Grid2D.cs
+++ Assets/Scripts/Pathfinding/Grid2D.cs
@@ -117,6 +117,111 @@ public class Grid2D : MonoBehaviour
         return Grid[x, y];
     }
 
+    //A* pathfinding algorithm
+    public List<Node2D> FindPath(Vector3 startPos, Vector3 targetPos)
+    {
+        Node2D seekerNode, targetNode;
+
+        //reset path
+        List<Node2D> path = new List<Node2D>();
+
+        //get player and target position in grid coords
+        seekerNode = NodeFromWorldPoint(startPos);
+        targetNode = NodeFromWorldPoint(targetPos);
+
+        //if already at target, skip calculations
+        if (seekerNode == targetNode)
+        {
+            return RetracePath(ref path, seekerNode, targetNode);
+        }
+
+        List<Node2D> openSet = new List<Node2D>();
+        HashSet<Node2D> closedSet = new HashSet<Node2D>();
+        openSet.Add(seekerNode);
+
+        //calculates path for pathfinding
+        while (openSet.Count > 0)
+        {
+            //iterates through openSet and finds lowest FCost
+            Node2D node = openSet[0];
+            for (int i = 1; i < openSet.Count; i++)
+            {
+                if (openSet[i].FCost <= node.FCost)
+                {
+                    if (openSet[i].hCost < node.hCost)
+                        node = openSet[i];
+                }
+            }
+
+            openSet.Remove(node);
+            closedSet.Add(node);
+
+            //If target found, retrace path
+            if (node == targetNode)
+            {
+                return RetracePath(ref path, seekerNode, targetNode);
+            }
+
+            //adds neighbor nodes to openSet
+            foreach (Node2D neighbour in GetNeighbors(node))
+            {
+                if (neighbour.obstacle || closedSet.Contains(neighbour))
+                {
+                    continue;
+                }
+
+                int newCostToNeighbour = node.gCost + GetDistance(node, neighbour);
+                if (newCostToNeighbour < neighbour.gCost || !openSet.Contains(neighbour))
+                {
+                    neighbour.gCost = newCostToNeighbour;
+                    neighbour.hCost = GetDistance(neighbour, targetNode);
+                    neighbour.parent = node;
+
+                    if (!openSet.Contains(neighbour))
+                    {
+                        openSet.Add(neighbour);
+                    }
+                }
+            }
+        }
+
+        //No path possible
+        return null;
+    }
+
+    //reverses calculated path so first node is closest to seeker
+    List<Node2D> RetracePath(ref List<Node2D> path, Node2D startNode, Node2D endNode)
+    {
+        Node2D currentNode = endNode;
+
+        //if path is 0
+        if (startNode == endNode)
+        {
+            return path;
+        }
+
+        while (currentNode != startNode)
+        {
+            path.Add(currentNode);
+            currentNode = currentNode.parent;
+        }
+        path.Reverse();
+        return path;
+    }
+
+    //gets distance between 2 nodes for calculating cost
+    int GetDistance(Node2D nodeA, Node2D nodeB)
+    {
+        int dstX = Mathf.Abs(nodeA.GridX - nodeB.GridX);
+        int dstY = Mathf.Abs(nodeA.GridY - nodeB.GridY);
+
+        if (dstX > dstY)
+        {
+            return 14 * dstY + 10 * (dstX - dstY);
+        }
+        return 14 * dstX + 10 * (dstY - dstX);
+    }
+
     //Draws visual representation of grid
     void OnDrawGizmos()
     {
@@ -134,4 +239,6 @@ public class Grid2D : MonoBehaviour
             Gizmos.DrawCube(n.worldPosition, Vector3.one * 0.9f * (nodeRadius));
         }
     }
+
+    
 }
