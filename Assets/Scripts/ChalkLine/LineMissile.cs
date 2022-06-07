65090d3f5a0cd3e574abc04bc5ea9a87964446f1
pixelfac <pixelfac.nerfbros@gmail.com>
2022-06-06 23:38:35 -0400

    namespace linemissile

--- Assets/Scripts/ChalkLine/LineMissile.cs
+++ Assets/Scripts/ChalkLine/LineMissile.cs
@@ -2,74 +2,77 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class LineMissile : MonoBehaviour
+namespace ChalkLine
 {
-	[SerializeField] private float _speed;
-
-	private GameObject _startCircle;
-	private GameObject _endCircle;
-
-	private float _strength;
-	private Vector2 _direction;
-	private LineRenderer _lr;
-	private EdgeCollider2D _hitbox;
-	private Rigidbody2D _rb;
-
-	private void Awake()
+	public class LineMissile : MonoBehaviour
 	{
-		_lr = GetComponent<LineRenderer>();
-		_hitbox = GetComponent<EdgeCollider2D>();
-		_rb = GetComponent<Rigidbody2D>();
-	}
+		[SerializeField] private float _speed;
 
-	//basically a constructor, but since can't call constructor
-	//on gameobject prefab component, this is the best alternative
-	public void Init(GameObject startCircle, GameObject endCircle, float length, LineRenderer lr)
-	{
+		private GameObject _startCircle;
+		private GameObject _endCircle;
 
-		transform.position = endCircle.transform.position;
-		Vector3 startPos = startCircle.transform.position - endCircle.transform.position;
-		Vector3 endPos = Vector3.zero;
+		private float _strength;
+		private Vector2 _direction;
+		private LineRenderer _lr;
+		private EdgeCollider2D _hitbox;
+		private Rigidbody2D _rb;
 
-		_direction = (endPos - startPos).normalized;
-
-		//set startCircle pos
-		_startCircle = startCircle;
-		_startCircle.transform.SetParent(transform);
-		_startCircle.transform.position = startCircle.transform.position;
-		//set endCircle pos
-		_endCircle = endCircle;
-		_endCircle.transform.SetParent(transform);
-		_endCircle.transform.position = endCircle.transform.position;
-
-		_strength = CalcStrength();
-
-		SetLR();
-		SetHitbox();
-
-		_rb.velocity = _direction * _speed;
-
-		float CalcStrength()
+		private void Awake()
 		{
-			return length;
+			_lr = GetComponent<LineRenderer>();
+			_hitbox = GetComponent<EdgeCollider2D>();
+			_rb = GetComponent<Rigidbody2D>();
 		}
 
-		//set line renderer values
-		void SetLR()
+		//basically a constructor, but since can't call constructor
+		//on gameobject prefab component, this is the best alternative
+		public void Init(GameObject startCircle, GameObject endCircle, float length, LineRenderer lr)
 		{
-			_lr.SetPositions(new Vector3[] { startPos, endPos });
-			_lr.endWidth = lr.endWidth;
-			_lr.startWidth = lr.startWidth;
-			_lr.startColor = lr.startColor;
-			_lr.endColor = lr.endColor;
-			_lr.materials = lr.materials;
-		}
 
-		//set edge collider values
-		void SetHitbox()
-		{
-			_hitbox.points = new Vector2[] { startPos, endPos };
-			_hitbox.edgeRadius = lr.startWidth * 2;
+			transform.position = endCircle.transform.position;
+			Vector3 startPos = startCircle.transform.position - endCircle.transform.position;
+			Vector3 endPos = Vector3.zero;
+
+			_direction = (endPos - startPos).normalized;
+
+			//set startCircle pos
+			_startCircle = startCircle;
+			_startCircle.transform.SetParent(transform);
+			_startCircle.transform.position = startCircle.transform.position;
+			//set endCircle pos
+			_endCircle = endCircle;
+			_endCircle.transform.SetParent(transform);
+			_endCircle.transform.position = endCircle.transform.position;
+
+			_strength = CalcStrength();
+
+			SetLR();
+			SetHitbox();
+
+			_rb.velocity = _direction * _speed;
+
+			float CalcStrength()
+			{
+				return length;
+			}
+
+			//set line renderer values
+			void SetLR()
+			{
+				_lr.SetPositions(new Vector3[] { startPos, endPos });
+				_lr.endWidth = lr.endWidth;
+				_lr.startWidth = lr.startWidth;
+				_lr.startColor = lr.startColor;
+				_lr.endColor = lr.endColor;
+				_lr.materials = lr.materials;
+			}
+
+			//set edge collider values
+			void SetHitbox()
+			{
+				_hitbox.points = new Vector2[] { startPos, endPos };
+				_hitbox.edgeRadius = lr.startWidth * 2;
+			}
 		}
-	}
+	} 
 }
