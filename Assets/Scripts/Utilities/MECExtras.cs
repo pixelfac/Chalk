f6f85b365375196a65729f9a0bebffbbcc61ed22
pixelfac <pixelfac.nerfbros@gmail.com>
2022-07-16 23:21:05 -0400

    MECExtras

---
+++ Assets/Scripts/Utilities/MECExtras.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using MEC;
+
+public class MECExtras : MonoBehaviour
+{
+	//calls 'action' after 'delay' seconds
+    public static void CallDelayed(Action action, float delay)
+	{
+		Timing.RunCoroutine(CallDelayedRoutine(action, delay));
+	}
+
+	private static IEnumerator<float> CallDelayedRoutine(Action action, float delay)
+	{
+		yield return Timing.WaitForSeconds(delay);
+		action?.Invoke();
+	}
+
+	//calls 'action' immediately, and then repeatedly on 'delay' second delay
+	public static void CallRepeating(Action action, float delay)
+	{
+		Timing.RunCoroutine(CallRepeatingRoutine(action, delay));
+	}
+
+	private static IEnumerator<float> CallRepeatingRoutine(Action action, float delay)
+	{
+		while (true)
+		{
+			action?.Invoke();
+			yield return Timing.WaitForSeconds(delay);
+		}
+	}
+}
